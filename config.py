fda6a6921d4f33f9972599071baab97d56ec14bb
ThomazPom <thomas.benhamou@hotmail.fr>
2021-02-08 13:13:53 +0100

    Now sell orders can retrieve orphan buy orders

--- config.py
+++ config.py
@@ -3,11 +3,16 @@ multiplier_price_win = 0
 use_maxprice = True
 invest = 5000
 assetpairwatch = "XDG/EUR"
-time_to_watch = 10
+time_to_watch = 300
 key_file = "key.key"
 ws_token = None
 enable_buys = True
 enable_sells = True
-precision = -1
+pair_decimals = -1
+lot_decimals = -1
+asset_pair=None
+
+on_cancel_reinvest_number = 1
 
 assetpair_no_slash = assetpairwatch.replace("/", "")
+buy_orders_too_high_file = "buy_orders_too_high.json"
